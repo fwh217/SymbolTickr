440df2f9a5d1378cb8ac309e996aacb3859bd1e5
Frank Hogan <frankhogan@Franks-MacBook-Pro-3.local>
2018-08-10 19:56:10 -0400

    converting from .NET Core App to .NET Standard Library and implementing DataTemplateSelector (mac)

---
+++ Mobile/SymbolTickr/SymbolTickr/Core/SymbolDataTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using QuoteStreamer.TickerInfo;
+
+namespace SymbolTickr.Core
+{
+	public class SymbolDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate StockTemplate { get; set; }
+        public DataTemplate CurrencyTemplate { get; set; }
+        public DataTemplate CryptoTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            DataTemplate template = null;
+            if (item is StockTickerInfo)
+            {
+                template = StockTemplate;
+            }
+            else if (item is CurrencyTickerInfo)
+            {
+                template = CurrencyTemplate;
+            }
+            else if (item is CryptoTickerInfo)
+            {
+                template = CryptoTemplate;
+            }
+            return template;
+        }
+    }
+}
